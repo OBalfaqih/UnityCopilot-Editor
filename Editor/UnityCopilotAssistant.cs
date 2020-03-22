43e26351d9c5b7127da4795b8c37af3bc1743718
Omar Balfaqih <obalfaqih@gmail.com>
2020-03-22 20:41:30 +0300

    Add files via upload

---
+++ Editor/UnityCopilotAssistant.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class UnityCopilotAssistant : MonoBehaviour
+{
+    [MenuItem("CONTEXT/Object/Go to Unity Copilot")]
+    static void GoToUnityCopilot(MenuCommand command)
+    {
+        string component = command.context.GetType().Name;
+        Application.OpenURL("https://unitycopilot.com/search?q=" + component + "#results");
+    }
+}
